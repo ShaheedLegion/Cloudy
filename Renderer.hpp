7fc05f89addc5582dcec471ec49ed021164d4629
shaheed.abdol <shaheed.abdol@playsafesa.com>
2014-11-13 11:18:20 +0200

    Created Renderer

---
+++ Renderer.hpp
@@ -0,0 +1,130 @@
+#pragma once
+#ifndef RENDERER_HPP_INCLUDED
+#define RENDERER_HPP_INCLUDED
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#define _WIDTH 256
+#define _HEIGHT 256
+#define _BPP 32
+
+namespace detail {
+
+class RendererThread {
+public:
+	RendererThread(LPTHREAD_START_ROUTINE callback) : m_running(false),
+		hThread(INVALID_HANDLE_VALUE), m_callback(callback) {}
+
+	~RendererThread() {
+		if (m_running)
+			Join();
+	}
+
+	void Start(LPVOID lParam) {
+		hThread = CreateThread(NULL, 0, m_callback, lParam, 0, NULL);
+		m_running = true;
+	}
+	void Join() {
+		if (m_running)
+			m_running = false;
+
+		WaitForSingleObject(hThread, INFINITE);
+		CloseHandle(hThread);
+		hThread = INVALID_HANDLE_VALUE;
+	}
+
+	void Delay(DWORD millis) {
+		// Try to delay for |millis| time duration.
+		// This is called from within the threading function (callback)
+		// So it's safe to sleep in the calling thread.
+		Sleep(millis);
+	}
+protected:
+	bool m_running;
+	HANDLE hThread;
+	LPTHREAD_START_ROUTINE m_callback;
+	//some protected stuff.
+};
+
+
+typedef unsigned int Uint32;
+
+class RendererSurface {
+public:
+	RendererSurface(int w, int h, int bpp) : m_w(w), m_h(h), m_bpp(bpp) {
+		m_pixels = new unsigned char[m_w * m_h * (m_bpp / 8)];
+		m_backBuffer = new unsigned char[m_w * m_h * (m_bpp / 8)];
+	}
+
+	~RendererSurface() {
+		delete [] m_pixels;
+		delete [] m_backBuffer;
+		m_pixels = NULL;
+		m_backBuffer = NULL;
+	}
+
+
+	void SetScreen(unsigned char * buffer, HDC screenDC, HDC memDC) {
+		m_screen = buffer;
+		m_screenDC = screenDC;
+		m_dc = memDC;
+	}
+
+	void Flip() {
+		//We need a mechanism to actually present the buffer to the drawing system.
+		unsigned char * temp = m_pixels;
+		m_pixels = m_backBuffer;
+		m_backBuffer = temp;
+
+		memcpy(m_screen, m_pixels, (m_w * m_h * (m_bpp / 8)));
+		BitBlt(m_screenDC, 0, 0, m_w, m_h, m_dc, 0, 0, SRCCOPY);
+	}
+
+	Uint32* GetPixels() {
+		return reinterpret_cast<Uint32*>(m_backBuffer);
+	}
+
+	int GetBPP() const {
+		return m_bpp;
+	}
+
+	int GetWidth() const {
+		return m_w;
+	}
+
+	int GetHeight() const {
+		return m_h;
+	}
+
+protected:
+	unsigned char* m_pixels;
+	unsigned char* m_backBuffer;
+	unsigned char* m_screen;
+	int m_w;
+	int m_h;
+	int m_bpp;
+	HDC m_screenDC;
+	HDC m_dc;
+};
+
+}  // namespace detail
+
+class Renderer
+{
+public:
+	detail::RendererSurface screen;
+	detail::RendererThread updateThread;
+
+    bool bRunning;
+
+	void SetBuffer(unsigned char * buffer, HDC scrDC, HDC memDC);
+
+public:
+    Renderer(const char* const className, LPTHREAD_START_ROUTINE callback);
+    ~Renderer();
+    bool IsRunning();
+    void SetRunning(bool);
+};
+
+#endif // RENDERER_HPP_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,163 @@
+#include <Windows.h>
+#include "Renderer.hpp"
+
+namespace _impl {
+// Fetched most of this code from: http://lodev.org/cgtutor/randomnoise.html
+
+#define noiseDepth 64
+
+#define RGB_S(b, g, r)                                                         \
+  ((COLORREF)(((BYTE)(r) | ((WORD)((BYTE)(g)) << 8)) |                         \
+              (((DWORD)(BYTE)(b)) << 16)))
+
+double noise[_WIDTH][_HEIGHT][noiseDepth]; // the noise array
+
+void generateNoise() {
+  for (int x = 0; x < _WIDTH; x++)
+    for (int y = 0; y < _HEIGHT; y++)
+      for (int z = 0; z < noiseDepth; z++) {
+        noise[x][y][z] = (rand() % 32768) / 32768.0;
+      }
+}
+
+double smoothNoise(double x, double y, double z) {
+  // get fractional part of x and y
+  double fractX = x - int(x);
+  double fractY = y - int(y);
+  double fractZ = z - int(z);
+
+  // wrap around
+  int x1 = (int(x) + _WIDTH) % _WIDTH;
+  int y1 = (int(y) + _HEIGHT) % _HEIGHT;
+  int z1 = (int(z) + noiseDepth) % noiseDepth;
+
+  // neighbor values
+  int x2 = (x1 + _WIDTH - 1) % _WIDTH;
+  int y2 = (y1 + _HEIGHT - 1) % _HEIGHT;
+  int z2 = (z1 + noiseDepth - 1) % noiseDepth;
+
+  // smooth the noise with bilinear interpolation
+  double value = 0.0;
+  value += fractX * fractY * fractZ * noise[x1][y1][z1];
+  value += fractX * (1 - fractY) * fractZ * noise[x1][y2][z1];
+  value += (1 - fractX) * fractY * fractZ * noise[x2][y1][z1];
+  value += (1 - fractX) * (1 - fractY) * fractZ * noise[x2][y2][z1];
+
+  value += fractX * fractY * (1 - fractZ) * noise[x1][y1][z2];
+  value += fractX * (1 - fractY) * (1 - fractZ) * noise[x1][y2][z2];
+  value += (1 - fractX) * fractY * (1 - fractZ) * noise[x2][y1][z2];
+  value += (1 - fractX) * (1 - fractY) * (1 - fractZ) * noise[x2][y2][z2];
+
+  return value;
+}
+
+double turbulence(double x, double y, double z, double size) {
+  double value = 0.0, initialSize = size;
+
+  while (size >= 1) {
+    value += smoothNoise(x / size, y / size, z / size) * size;
+    size /= 2.0;
+  }
+
+  return (128.0 * value / initialSize);
+}
+
+typedef unsigned char uchar;
+
+detail::Uint32 HSLtoRGB(uchar _h, uchar _s, uchar _l) {
+  float r, g, b, h, s, l; // this function works with floats between 0 and 1
+  float temp1, temp2, tempr, tempg, tempb;
+  h = _h / 256.0;
+  s = _s / 256.0;
+  l = _l / 256.0;
+  // If saturation is 0, the color is a shade of gray
+  if (s == 0)
+    r = g = b = l;
+  // If saturation > 0, more complex calculations are needed
+  else {
+    // Set the temporary values
+    if (l < 0.5)
+      temp2 = l * (1 + s);
+    else
+      temp2 = (l + s) - (l * s);
+    temp1 = 2 * l - temp2;
+    tempr = h + 1.0 / 3.0;
+    if (tempr > 1)
+      tempr--;
+    tempg = h;
+    tempb = h - 1.0 / 3.0;
+    if (tempb < 0)
+      tempb++;
+
+    // Red
+    if (tempr < 1.0 / 6.0)
+      r = temp1 + (temp2 - temp1) * 6.0 * tempr;
+    else if (tempr < 0.5)
+      r = temp2;
+    else if (tempr < 2.0 / 3.0)
+      r = temp1 + (temp2 - temp1) * ((2.0 / 3.0) - tempr) * 6.0;
+    else
+      r = temp1;
+
+    // Green
+    if (tempg < 1.0 / 6.0)
+      g = temp1 + (temp2 - temp1) * 6.0 * tempg;
+    else if (tempg < 0.5)
+      g = temp2;
+    else if (tempg < 2.0 / 3.0)
+      g = temp1 + (temp2 - temp1) * ((2.0 / 3.0) - tempg) * 6.0;
+    else
+      g = temp1;
+
+    // Blue
+    if (tempb < 1.0 / 6.0)
+      b = temp1 + (temp2 - temp1) * 6.0 * tempb;
+    else if (tempb < 0.5)
+      b = temp2;
+    else if (tempb < 2.0 / 3.0)
+      b = temp1 + (temp2 - temp1) * ((2.0 / 3.0) - tempb) * 6.0;
+    else
+      b = temp1;
+  }
+
+  return RGB_S((r * 255.0), (g * 255.0), (b * 255.0));
+}
+
+} // namespace _impl
+
+
+// This is the guts of the renderer, without this it will do nothing.
+DWORD WINAPI Update(LPVOID lpParameter) { // poll for some kind of event here to
+                                          // signal that we want to exit.
+  _impl::generateNoise();
+  double t(0.0);
+  Renderer* g_renderer = static_cast<Renderer*>(lpParameter);
+
+  while (g_renderer->IsRunning()) {
+
+    detail::Uint32 *buffer = g_renderer->screen.GetPixels();
+
+    for (int x = 0; x < _WIDTH; x++) {
+      for (int y = 0; y < _HEIGHT; y++) {
+        unsigned int L(192 +
+                       (unsigned char)(_impl::turbulence(x, y, t, 32)) / 4);
+        *buffer = _impl::HSLtoRGB(169, 255, L);
+        buffer++;
+      }
+    }
+    t += 12.0;
+
+    g_renderer->screen.Flip();
+    g_renderer->updateThread.Delay(1);
+  }
+
+  return 0;
+}
+
+
+int main(int argc, char *args[]) {
+  const char *const myclass = "Cloudy";
+  Renderer renderer(myclass, &Update);
+
+  return 0;
+}
